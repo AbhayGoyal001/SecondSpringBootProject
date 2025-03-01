9e8e01117615c7ae5a982b82338d5c71fb165468
AbhayGoyal001 <abhaygoyalscience@gmail.com>
2025-03-02 00:01:28 +0530

    [Abhay Goyal] Added  LoggerFunctionality

--- src/main/java/org/example/secondspringbootproject/SpringConceptsApplication.java
+++ src/main/java/org/example/secondspringbootproject/SpringConceptsApplication.java
@@ -12,7 +12,11 @@ public class SpringConceptsApplication {
     public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
     public static void main(String[] args) {
         logger.debug ("Welcome to Spring Concept Demo");
+        logger.info("Starting Spring Boot Application...");
         ApplicationContext context = SpringApplication. run (SpringConceptsApplication.class, args);
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        demoBean.sayHello();
+        logger.info("Application Started Successfully!");
         logger.debug ("Checking Context: {}", context.getBean (DemoBean.class) );
         logger.debug("\n*** Example Using @Autowire annotation on property ***");
     }
